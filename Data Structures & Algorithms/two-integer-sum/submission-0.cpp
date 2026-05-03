cb7aa704e138315b88352f0c81adcb83af443346
Swarnadeep Barik <rite2swarnadeep@gmail.com>
2026-05-03 19:02:16 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+     unordered_map<int, int> diff;
+        for(int i=0;i<nums.size();i++)
+        {
+            int complement=target-nums[i];
+            if(diff.find(complement) != diff.end()){
+               return {diff[complement], i};
+            }
+            diff.insert({nums[i], i});
+        }
+    }
+};
